08dde81062650dab16adcbc3f57aaee634eff3f9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomCheckComboBox/MainWindow.xaml.vb
@@ -0,0 +1,67 @@
+﻿' Developer Express Code Central Example:
+' How to allow ComboBoxEdit to select multiple items with the autocomplete feature
+' 
+' By default, ComboBoxEdit allows you to select one item using the autocomplete
+' feature.
+' This example provides a way to enable the behavior when the
+' autocomplete feature works for multiple item selection. Firstly, it is necessary
+' to create a ComboBoxEdit descendant and override the CreateEditStrategy method,
+' which should return your own ComboBoxEditStrategy class descendant.
+' Then,
+' override and implement the ProcessChangeText method, and some more methods to
+' enable item auto-searching selection.
+' For unimpeded search and selection of
+' items without losing input field focus, the set FocusPopupOnOpen property to
+' False.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4226
+
+' Developer Express Code Central Example:
+' How to allow ComboBoxEdit to select multiple items with the autocomplete feature
+' 
+' By default, ComboBoxEdit allows you to select one item using the autocomplete
+' feature.
+' This example provides a way to enable the behavior when the
+' autocomplete feature works for multiple item selection. Firstly, it is necessary
+' to create a ComboBoxEdit descendant and override the CreateEditStrategy method,
+' which should return your own ComboBoxEditStrategy class descendant.
+' Then,
+' override and implement the ProcessChangeText method, and some more methods to
+' enable item auto-searching selection.
+' For unimpeded search and selection of
+' items without losing input field focus, the set FocusPopupOnOpen property to
+' False.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4226
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows
+
+Namespace CustomCheckComboBox
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            FillCombo()
+        End Sub
+
+        Public Sub FillCombo()
+            Dim persons As New List(Of Person)()
+            persons.Add(New Person() With {.Id = 1, .IsActive = False, .Name = "Adam"})
+            persons.Add(New Person() With {.Id = 2, .IsActive = True, .Name = "John"})
+            persons.Add(New Person() With {.Id = 3, .IsActive = False, .Name = "Alexander"})
+            persons.Add(New Person() With {.Id = 4, .IsActive = True, .Name = "Andrew"})
+            persons.Add(New Person() With {.Id = 5, .IsActive = True, .Name = "Alexey"})
+            persons.Add(New Person() With {.Id = 6, .IsActive = True, .Name = "Michael"})
+
+            multiSelectionCombo.ItemsSource = persons
+            multiSelectionCombo.DisplayMember = "Name"
+            multiSelectionCombo.ValueMember = "Id"
+        End Sub
+    End Class
+End Namespace
---
+++ VB/CustomCheckComboBox/Person.vb
@@ -0,0 +1,53 @@
+﻿' Developer Express Code Central Example:
+' How to allow ComboBoxEdit to select multiple items with the autocomplete feature
+' 
+' By default, ComboBoxEdit allows you to select one item using the autocomplete
+' feature.
+' This example provides a way to enable the behavior when the
+' autocomplete feature works for multiple item selection. Firstly, it is necessary
+' to create a ComboBoxEdit descendant and override the CreateEditStrategy method,
+' which should return your own ComboBoxEditStrategy class descendant.
+' Then,
+' override and implement the ProcessChangeText method, and some more methods to
+' enable item auto-searching selection.
+' For unimpeded search and selection of
+' items without losing input field focus, the set FocusPopupOnOpen property to
+' False.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4226
+
+' Developer Express Code Central Example:
+' How to allow ComboBoxEdit to select multiple items with the autocomplete feature
+' 
+' By default, ComboBoxEdit allows you to select one item using the autocomplete
+' feature.
+' This example provides a way to enable the behavior when the
+' autocomplete feature works for multiple item selection. Firstly, it is necessary
+' to create a ComboBoxEdit descendant and override the CreateEditStrategy method,
+' which should return your own ComboBoxEditStrategy class descendant.
+' Then,
+' override and implement the ProcessChangeText method, and some more methods to
+' enable item auto-searching selection.
+' For unimpeded search and selection of
+' items without losing input field focus, the set FocusPopupOnOpen property to
+' False.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4226
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+
+Namespace CustomCheckComboBox
+    Friend Class Person
+        Public Property Name() As String
+        Public Property Id() As Integer
+        Public Property IsActive() As Boolean
+        Public Overrides Function ToString() As String
+            Return Name
+        End Function
+    End Class
+End Namespace
