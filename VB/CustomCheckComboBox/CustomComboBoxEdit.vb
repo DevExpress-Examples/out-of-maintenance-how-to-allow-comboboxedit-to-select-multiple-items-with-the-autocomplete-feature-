9ab00024272103b53f882914453033aafe612d8e
DevExpressExampleBot <fakeEmail>
2021-10-06 15:40:15 +0300

    autogenerate VB

--- VB/CustomCheckComboBox/CustomComboBoxEdit.vb
+++ VB/CustomCheckComboBox/CustomComboBoxEdit.vb
@@ -1,19 +1,16 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports DevExpress.Xpf.Editors
 
 Namespace CustomCheckComboBox
-	Public Class CustomComboBoxEdit
-		Inherits ComboBoxEdit
 
-		Shared Sub New()
-			CustomComboBoxEditSettings.RegisterCustomEdit()
-		End Sub
+    Public Class CustomComboBoxEdit
+        Inherits ComboBoxEdit
 
-		Protected Overrides Function CreateEditStrategy() As EditStrategyBase
-			Return New CustomEditStrategy(Me)
-		End Function
-	End Class
+        Shared Sub New()
+            CustomComboBoxEditSettings.RegisterCustomEdit()
+        End Sub
+
+        Protected Overrides Function CreateEditStrategy() As EditStrategyBase
+            Return New CustomEditStrategy(Me)
+        End Function
+    End Class
 End Namespace
